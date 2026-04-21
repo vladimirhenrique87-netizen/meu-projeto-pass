f65b7b85cb83f5afe45739c3985304e4594edb63
vladimirhenrique87-netizen <vladimirhenrique87@gmail.com>
2026-04-21 20:08:43 -0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 import os
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "<h1>Projeto PaaS Funcionando</h1>"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
